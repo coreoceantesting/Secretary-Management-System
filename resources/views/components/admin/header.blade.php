0195ee7d7bcde8689f8381efdc42a5d963985a88
Core Ocean <coreoceantesting@gmail.com>
2024-04-16 14:48:25 +0530

    fix header

--- resources/views/components/admin/header.blade.php
+++ resources/views/components/admin/header.blade.php
@@ -32,14 +32,14 @@
                 </button>
 
                 <!-- App Search-->
-                <form class="app-search d-none d-md-block">
+                {{-- <form class="app-search d-none d-md-block">
                     <div class="position-relative">
                         <input type="text" class="form-control" placeholder="Search..." autocomplete="off" id="search-options" value="" />
                         <span class="mdi mdi-magnify search-widget-icon"></span>
                         <span class="mdi mdi-close-circle search-widget-icon search-widget-icon-close d-none" id="search-close-options"></span>
                     </div>
 
-                </form>
+                </form> --}}
             </div>
 
             <div class="d-flex align-items-center">
@@ -68,11 +68,11 @@
                     </button>
                 </div>
 
-                <div class="ms-1 header-item d-none d-sm-flex">
+                {{-- <div class="ms-1 header-item d-none d-sm-flex">
                     <button type="button" id="change-theme-button" class="btn btn-icon btn-topbar btn-ghost-secondary rounded-circle light-dark-mode">
                         <i class="bx bx-moon fs-22"></i>
                     </button>
-                </div>
+                </div> --}}
 
 
                 <div class="dropdown ms-sm-3 header-item topbar-user">
