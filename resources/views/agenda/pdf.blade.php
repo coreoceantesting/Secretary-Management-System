26179001bb709dcf20eae64b791073b8cd0b15d7
= <=>
2025-06-16 18:02:38 +0530

    done pdf indexed design pdf

--- resources/views/agenda/pdf.blade.php
+++ resources/views/agenda/pdf.blade.php
@@ -1,129 +1,116 @@
 <!DOCTYPE html>
 <html lang="mr">
+
 <head>
     <meta charset="UTF-8">
     <title>सभेची नोटीस</title>
     <style>
         body {
             font-family: 'freeserif', sans-serif;
-            margin: 0;
-            padding: 0;
             font-size: 16px;
+            margin: 0;
+            padding: 20px;
             line-height: 1.6;
         }
 
-        .page-border {
-            border: 3px solid #000;
-            padding: 25px;
-            margin: 15px;
-        }
-
-        .header {
-            text-align: center;
-            margin-bottom: 20px;
-        }
-
-        .logo-and-header {
-            text-align: center;
-        }
-
-        .logo {
-            width: 100px;
-            height: auto;
-            margin-bottom: 10px;
-        }
-
-        .title-1 { font-size: 22px; font-weight: bold; display: block; }
-        .title-2 { font-size: 20px; display: block; }
-        .title-3 { font-size: 18px; display: block; }
-        .title-4 { font-size: 18px; display: block; }
-
-        table {
+        #header table {
             width: 100%;
-            margin-top: 15px;
-            border-collapse: collapse;
         }
 
-        table td {
-            padding: 4px 0;
-            vertical-align: top;
+        .text-center {
+            text-align: center;
+        }
+
+        .text-right {
+            text-align: right;
+        }
+
+        .header-logo {
+            width: 120px;
+            height: auto;
+        }
+
+        .subject-title {
+            font-size: 20px;
+            font-weight: bold;
+            margin: 25px 0 10px;
+            text-align: center;
         }
 
         p {
             text-align: justify;
-            margin: 10px 0;
+            margin: 8px 0;
         }
 
-        .meeting-info {
-            margin-top: 15px;
-        }
-
-        .subject {
-            text-align: center;
-            font-size: 20px;
-            font-weight: bold;
-            margin-top: 25px;
-            margin-bottom: 10px;
+        table td {
+            vertical-align: top;
         }
 
         .footer {
-            text-align: right;
             margin-top: 40px;
+            text-align: right;
             font-weight: bold;
         }
     </style>
 </head>
-<body>
-    <div class="page-border">
-        <!-- Logo and Header (vertically stacked) -->
-        <div class="logo-and-header">
-            <img src="{{ public_path('admin/images/PMC-logo.png') }}" alt="PMC Logo" class="logo">
-            <span class="title-1">पनवेल महानगरपालिका</span>
-            <span class="title-2">सभेची नोटीस</span>
-            <span class="title-3">स्थायी समिती सभा कामकाज पार पाडण्याबाबत</span>
-            <span class="title-3">प्रशासकाची सभा क्र.४७/११४</span>
-            <span class="title-4">सोमवार दिनांक : {{ date('d/m/Y', strtotime($agenda->date)) }}</span>
-        </div>
 
-        <!-- Reference Details Table -->
+<body>
+    <!-- Header -->
+    <section id="header">
         <table>
             <tr>
-                <td style="width: 80%;">जा.क्र.पमपा./सचिव/१९-२४/प्र.क्र.७२/१५/२४</td>
-                <td style="text-align: right;">दिनांक : {{ date('d/m/Y', strtotime($agenda->date)) }}</td>
+                <td style="width: 20%">
+                    @php
+                        $logoData = file_get_contents(public_path('admin/images/PMC-logo.png'));
+                        $base64Logo = base64_encode($logoData);
+                    @endphp
+                    <img src="data:image/png;base64,{{ $base64Logo }}" class="header-logo" alt="PMC Logo">
+                </td>
+                <td class="text-center" style="width: 60%">
+                    <h2 style="margin: 0;">पनवेल महानगरपालिका</h2>
+                    <h3 style="margin: 0;">सभेची नोटीस</h3>
+                    <p style="margin: 0;">स्थायी समिती सभा कामकाज पार पाडण्याबाबत</p>
+                    <p style="margin: 0;">प्रशासकाची सभा क्र.४७/११४</p>
+                    <p style="margin: 5px 0; font-size: 18px;"><b>सोमवार दिनांक : {{ date('d/m/Y', strtotime($agenda->date)) }}</b></p>
+                </td>
+                <td class="text-right" style="width: 20%">
+                    <p>दिनांक : {{ date('d/m/Y', strtotime($agenda->date)) }}</p>
+                </td>
             </tr>
         </table>
+    </section>
 
-        <!-- Body Paragraphs -->
-        <p>
-            ज्या अर्थी, महाराष्ट्र महानगरपालिका अधिनियमातील तरतुदीनुसार विविध कामकाज पार पाहण्यासाठी काही प्रस्तावांना स्थायी समितीची पूर्व मान्यता घेणे आवश्यक आहे आणि,
-        </p>
-        <p>
-            ज्या अर्थी, पनवेल महानगरपालिकेची मुदत दिनांक ०९ जुलै, २०२२ रोजी संपलेली असल्याने सद्यः स्थितीत स्थायी समिती अस्तित्वात नाही. आणि,
-        </p>
-        <p>
-            ज्या अर्थी, महाराष्ट्र महानगरपालिका अधिनियमाखाली सर्व अधिकारांचा वापर करण्यासाठी आणि तिची सर्व कामे व कर्तव्ये पार पाडण्यासाठी महाराष्ट्र महानगरपालिका अधिनियमातील तरतुदी व विशेषतः कलम ४५२ 'अ' च्या (१ अ) व (१ ब) मधील तरतुदींनुसार पनवेल महानगरपालिका येथे प्रशासक पदी आयुक्त, पनवेल महानगरपालिका यांची नियुक्ती महाराष्ट्र शासनाने केलेली आहे,
-        </p>
-        <p>
-            त्या अर्थी, स्थायी समितीच्या मान्यतेने पार पाडावयाचे कामकाज पूर्ण करण्यासाठी प्रशासक यांच्या अध्यक्षतेखाली खालील प्रस्तावांवर निर्णय घेणेकरीता बैठकीचे आयोजन करण्यात येत आहे.
-        </p>
+    <!-- Ref No. Table -->
+    <table style="width: 100%; margin-top: 20px;">
+        <tr>
+            <td style="width: 80%;">जा.क्र.पमपा./सचिव/१९-२४/प्र.क्र.७२/१५/२४</td>
+            <td class="text-right">दिनांक : {{ date('d/m/Y', strtotime($agenda->date)) }}</td>
+        </tr>
+    </table>
 
-        <!-- Meeting Info -->
-        <div class="meeting-info">
-            <p><b>बैठकीचे स्थळ :</b> {{ $agenda->place }}</p>
-            <p><b>दिनांक :</b> {{ date('d/m/Y', strtotime($agenda->date)) }}</p>
-            <p><b>वेळ :</b> {{ date('h:i A', strtotime($agenda->time)) }}</p>
-        </div>
+    <!-- Paragraphs -->
+    <p>ज्या अर्थी, महाराष्ट्र महानगरपालिका अधिनियमातील तरतुदीनुसार विविध कामकाज पार पाहण्यासाठी काही प्रस्तावांना स्थायी समितीची पूर्व मान्यता घेणे आवश्यक आहे आणि,</p>
 
-        <!-- Subject List -->
-        <div class="subject">विषय सूची</div>
-        @foreach($goshwaras as $goshwara)
-            <p>{{ $loop->iteration }}. {{ $goshwara?->goshwara?->subject }}</p>
-        @endforeach
+    <p>ज्या अर्थी, पनवेल महानगरपालिकेची मुदत दिनांक ०९ जुलै, २०२२ रोजी संपलेली असल्याने सद्यः स्थितीत स्थायी समिती अस्तित्वात नाही. आणि,</p>
 
-        <!-- Footer -->
-        <div class="footer">
-            मा. प्रशासकाकडील <br> कार्यालयीन कामकाज
-        </div>
+    <p>ज्या अर्थी, महाराष्ट्र महानगरपालिका अधिनियमाखाली सर्व अधिकारांचा वापर करण्यासाठी आणि तिची सर्व कामे व कर्तव्ये पार पाडण्यासाठी महाराष्ट्र महानगरपालिका अधिनियमातील तरतुदी व विशेषतः कलम ४५२ 'अ' च्या (१ अ) व (१ ब) मधील तरतुदींनुसार पनवेल महानगरपालिका येथे प्रशासक पदी आयुक्त, पनवेल महानगरपालिका यांची नियुक्ती महाराष्ट्र शासनाने केलेली आहे,</p>
+
+    <p>त्या अर्थी, स्थायी समितीच्या मान्यतेने पार पाडावयाचे कामकाज पूर्ण करण्यासाठी प्रशासक यांच्या अध्यक्षतेखाली खालील प्रस्तावांवर निर्णय घेणेकरीता बैठकीचे आयोजन करण्यात येत आहे.</p>
+
+    <!-- Meeting Info -->
+    <p><b>बैठकीचे स्थळ :</b> {{ $agenda->place }}</p>
+    <p><b>दिनांक :</b> {{ date('d/m/Y', strtotime($agenda->date)) }}</p>
+    <p><b>वेळ :</b> {{ date('h:i A', strtotime($agenda->time)) }}</p>
+
+    <!-- Subject List -->
+    <div class="subject-title">विषय सूची</div>
+    @foreach($goshwaras as $goshwara)
+        <p>{{ $loop->iteration }}. {{ $goshwara?->goshwara?->subject }}</p>
+    @endforeach
+
+    <!-- Footer -->
+    <div class="footer">
+        मा. प्रशासकाकडील <br> कार्यालयीन कामकाज
     </div>
 </body>
 </html>
