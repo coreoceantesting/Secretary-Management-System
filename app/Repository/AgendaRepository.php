c5eaadfe0308ffd03f388daf82f24317d5e800a3
SakshiChavan <sakshi.chavan@coreocean.co.in>
2026-05-08 11:41:19 +0530

    changes

--- app/Repository/AgendaRepository.php
+++ app/Repository/AgendaRepository.php
@@ -288,8 +288,9 @@ class AgendaRepository
                     if ($goshwara->goshwara && $goshwara->goshwara->file) {
                         // Check both storage/app/public and storage/app paths
                         $goshwaraPath = storage_path('app/public/'.str_replace('/', DIRECTORY_SEPARATOR, trim($goshwara->goshwara->file)));
-                        dd($goshwaraPath);
+                        
                         if (! file_exists($goshwaraPath)) {
+                            dd(8);
                             $goshwaraPath = storage_path('app/'.str_replace('/', DIRECTORY_SEPARATOR, trim($goshwara->goshwara->file)));
 
                         }
